1726f17283632fe7daddd72a45ba8238bd1456c4
Kemyl Sandid <156251659+Kemyyl@users.noreply.github.com>
2025-03-31 11:53:45 +0200

    ajout page error

---
+++ MedManager/Controllers/ErrorController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPBookProject.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
